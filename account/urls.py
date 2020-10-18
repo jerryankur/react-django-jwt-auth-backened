e4cff53248b90accd5a0c96c4d8bd8691a75906e
Priyanshu Dwivedi <ankurdwivedi75@gmail.com>
2020-10-18 18:22:56 +0530

    added the api to determine the current user
    determine the current user by their token and return their data

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
+from account.views import current_user
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
+from .views import current_user
 
 urlpatterns = [
-	path('user-auth/', obtain_jwt_token)
+	path('user-auth/', obtain_jwt_token),
+	path('current-user/', current_user),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
+@api_view(['GET'])
+def current_user(request):			# Determine the current user by their token, and return their data
+
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
