9a250674a4202498d4d2cb3a26113664a7874ce2
Priyanshu Dwivedi <ankurdwivedi75@gmail.com>
2020-10-18 20:50:22 +0530

    created "create_user" api it created the user id and returns the valid token and userdata with it

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from account.views import current_user
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
-from .views import current_user
+from .views import create_user, current_user
 
 urlpatterns = [
 	path('user-auth/', obtain_jwt_token),
 	path('current-user/', current_user),
+	path('users/', create_user)
 ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,29 @@
 
+from collections import UserList
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserSerializerWithToken
+from rest_framework import permissions, status
+from rest_framework.views import APIView
+
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 
 @api_view(['GET'])
 def current_user(request):			# Determine the current user by their token, and return their data
 
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
+
+class UserList(APIView):			# 'UserList' because normally we'd have a get method here too, for retrieving a list of all User objects.
+
+    permission_classes = (permissions.AllowAny,)		#permissions.AllowAny, because otherwise, the user would have to be logged in before they could sign up
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+create_user = UserList.as_view()
